f42dab517756ad322b1900410db9c2008494d84c
alancptk7 <alancptk.7@gmail.com>
2023-04-26 21:00:17 -0300

    corrections

--- src/controller/insert.php
+++ src/controller/insert.php
@@ -1,9 +1,10 @@
 <?php
 
+require_once '../config/connection.php';
+
 if (isset($_POST['id']) && !empty($_POST['id']) && isset($_POST['username']) && !empty($_POST['username'])) {
 
-    require '../config/connection.php';
-    require '../models/Usuario.php';
+    require_once '../models/Usuario.php';
 
     $user = new Usuario($_POST['id'], $_POST['username']);
 
